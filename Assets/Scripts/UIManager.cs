fde1f66b450c4e48e438f4e92ecd35017ae2c974
jrguez03 <jokorguez@gmail.com>
2025-01-25 23:10:48 +0000

    UI avanzada

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,22 +1,109 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.Interactions;
 using UnityEngine.InputSystem.LowLevel;
+using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
-    public float button;
+    [Header("UI Canvas")]
+    public GameObject mainMenu;
+    public GameObject settings;
+    public GameObject pause;
+    public bool paused;
+    public bool settingsMenu;
 
-    public void UIController(InputAction.CallbackContext context)
+    private void Awake()
     {
-        button = context.ReadValue<float>();
+        paused = false;
+        settingsMenu = false;
     }
 
-    public void Update()
+    public void InputActions(InputAction.CallbackContext context)
     {
-        if (button != 0)
+        if (context.ReadValueAsButton())
         {
-            
+            if (settingsMenu == true && SceneManager.GetSceneByBuildIndex(1) != null)
+            {
+                DesactivateSettingsUI();
+            }
         }
+        if (context.started)
+        {
+            if (paused == false && SceneManager.GetSceneByBuildIndex(1) != null && settingsMenu == false)
+            {
+                PauseLVL();
+            }
+        }
+    }
+
+    //Load the level scene when player press the play button
+    public void LoadLevelScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    //Load the main menu scene when player press the main menu button in the pause pop up
+    public void LoadMenuScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    //Active the UI settings
+    public void ActivateSettingsUI()
+    {
+        if (mainMenu != null)
+        {
+            settingsMenu = true;
+            mainMenu.SetActive(false);
+            settings.SetActive(true);
+        }
+
+        if (pause != null)
+        {
+            settingsMenu = true;
+            pause.SetActive(false);
+            settings.SetActive(true);
+        }
+    }
+
+    //Desactivate the UI settings
+    public void DesactivateSettingsUI()
+    {
+        //Main menu settings
+        if (SceneManager.GetSceneByBuildIndex(0) != null && settings != null)
+        {
+            settingsMenu = false;
+            settings.SetActive(false);
+            mainMenu.SetActive(true);
+        }
+        //Pause menu settings
+        if (SceneManager.GetSceneByBuildIndex(1) != null && settings != null)
+        {
+            settingsMenu = false;
+            settings.SetActive(false);
+            pause.SetActive(true);
+        }
+    }
+
+    //Quit the game
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    //Open pause menu
+    public void PauseLVL()
+    {
+        paused = true;
+        pause.SetActive(true);
+    }
+
+    //Close pause menu
+    public void ResumeLVL()
+    {
+        paused = false;
+        pause.SetActive(false);
     }
 }
