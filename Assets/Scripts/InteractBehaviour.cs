ca897d0d7e47fde7404ebb23498fb6de69a9ad2a
Rubiio14 <147033219+Rubiio14@users.noreply.github.com>
2025-01-25 16:48:11 +0000

    RecorridoBurbuja

--- Assets/Scripts/InteractBehaviour.cs
+++ Assets/Scripts/InteractBehaviour.cs
@@ -3,17 +3,38 @@ using UnityEngine.InputSystem;
 
 public class InteractBehaviour : MonoBehaviour
 {
+    public BubbleBehaviour bubbleScript;
+    public bool canInteract = false;
     public void OnInteract(InputAction.CallbackContext context)
     {
         // Ejecutar solo cuando la acción comience
         if (context.canceled)
         {
-            interactAction();
+            if (canInteract)
+            {
+                interactAction();
+            }
+            
         }
     }
 
     private void interactAction()
     {
-        print("Interactúa");
+        bubbleScript.nextWaypoint();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Si el jugador entra en el trigger
+        if (other.CompareTag("activateTrigger"))
+        {
+            canInteract = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        canInteract = false;
+        // Si el jugador entra en el trigger
+        Debug.Log("Sale");
     }
 }
--- Assets/Scripts/bubbleBehaviour.cs
+++ Assets/Scripts/bubbleBehaviour.cs
@@ -1,38 +1,25 @@
 using UnityEngine;
 
-public class bubbleBehaviour : MonoBehaviour
+public class BubbleBehaviour : MonoBehaviour
 {
-    //lugares hacia los que se mueve y velocidad
     [SerializeField] private Transform[] _waypoints;
     [SerializeField] private float _movSpeed = 2.5f;
-    private int currentWaypoint;
+    private int currentWaypoint = 0;
 
-    //Si es volador no activara la rotacion. Si no lo es, rotará al lado contrario
-    [SerializeField] private bool enemyFlying;
-    private bool needRotation;
-
-    private void Start()
+    void Start()
     {
-        currentWaypoint = 0;
+        transform.position = _waypoints[currentWaypoint].position;
     }
-    public void bubbleMovement()
+    void Update()
     {
         if (transform.position != _waypoints[currentWaypoint].position)
         {
             transform.position = Vector3.MoveTowards(transform.position, _waypoints[currentWaypoint].position, _movSpeed * Time.deltaTime);
         }
-        else
-        {
-            if (enemyFlying == false)
-            {
-                needRotation = true;
-            }
-            currentWaypoint = (currentWaypoint + 1) % _waypoints.Length;
-            if (needRotation == true)
-            {
-                transform.Rotate(0, 180, 0);
-                needRotation = false;
-            }
-        }
     }
+
+    public void nextWaypoint()
+    { 
+        currentWaypoint++;
+    }  
 }
