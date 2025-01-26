cf3658faad1c742c67ab4d5500b73789e69158f2
jrguez03 <jokorguez@gmail.com>
2025-01-26 16:01:55 +0000

    prefab de video final y sonido de puerta

--- Assets/Scripts/BlockedDoorBehaviour.cs
+++ Assets/Scripts/BlockedDoorBehaviour.cs
@@ -1,10 +1,13 @@
 using UnityEngine;
+using FMOD.Studio;
 
 public class DoorController : MonoBehaviour
 {
     public float targetRotation = -112f;  // Ángulo de rotación objetivo
     public float rotationSpeed = 1f;      // Velocidad de rotación (en grados por segundo)
     public bool canOpen = false;
+
+    public EventInstance door;
     
 
     void Update()
@@ -14,6 +17,7 @@ public class DoorController : MonoBehaviour
             if (targetRotation < gameObject.transform.rotation.y)
             {
                 transform.Rotate(0, -(1f * rotationSpeed * Time.deltaTime), 0);
+                door = AudioManager.instance.CreateInstance(FMODEvents.instance.AbrirPuerta);
             }
         }       
     }
